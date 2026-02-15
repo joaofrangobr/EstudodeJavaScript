240cfa9c4a739190f65f55e4878088d8e52b3e03
joaofrangobr <joaorobertozucon85@gmail.com>
2026-02-15 02:00:04 -0300

    Add basic math functions with examples
    Create a new file Estudos Js/function-calc.js that defines four simple calculator functions: CalcSum, CalcSub, CalcMult, and CalcDiv. Each function logs the result of the corresponding operation to the console and includes an example call demonstrating its usage.

---
+++ Estudos Js/function-calc.js
@@ -0,0 +1,24 @@
+function CalcSum(num1, num2) { // Aqui temos uma função chamada "CalcSum" que recebe dois parâmetros: "num1" e "num2". Esses parâmetros são usados para passar valores para a função quando ela é chamada.
+    console.log(num1 + num2); // Dentro da função, usamos console.log para imprimir a soma dos dois parâmetros "num1" e "num2" no console.
+}
+
+CalcSum(5, 10); // Aqui estamos chamando a função "CalcSum" e passando os valores 5 e 10 como argumentos. A função irá calcular a soma desses dois números e imprimir o resultado no console, que será 15.
+
+
+function CalcSub(num1, num2) { // Aqui temos uma função chamada "CalcSub" que recebe dois parâmetros: "num1" e "num2". Esses parâmetros são usados para passar valores para a função quando ela é chamada.
+    console.log(num1 - num2); // Dentro da função, usamos console.log para imprimir a subtração dos dois parâmetros "num1" e "num2" no console.
+}
+
+CalcSub(10, 5); // Aqui estamos chamando a função "CalcSub" e passando os valores 10 e 5 como argumentos. A função irá calcular a subtração desses dois números e imprimir o resultado no console, que será 5.
+
+
+function CalcMult(num1, num2) { // Aqui temos uma função chamada "CalcMult" que recebe dois parâmetros: "num1" e "num2". Esses parâmetros são usados para passar valores para a função quando ela é chamada.
+    console.log(num1 * num2); // Dentro da função, usamos console.log para imprimir a multiplicação dos dois parâmetros "num1" e "num2" no console.
+}
+CalcMult(5, 10); // Aqui estamos chamando a função "CalcMult" e passando os valores 5 e 10 como argumentos. A função irá calcular a multiplicação desses dois números e imprimir o resultado no console, que será 50.
+
+
+function CalcDiv(num1, num2) { // Aqui temos uma função chamada "CalcDiv" que recebe dois parâmetros: "num1" e "num2". Esses parâmetros são usados para passar valores para a função quando ela é chamada.
+    console.log(num1 / num2); // Dentro da função, usamos console.log para imprimir a divisão dos dois parâmetros "num1" e "num2" no console.
+}
+CalcDiv(10, 5); // Aqui estamos chamando a função "CalcDiv" e passando os valores 10 e 5 como argumentos. A função irá calcular a divisão desses dois números e imprimir o resultado no console, que será 2.
