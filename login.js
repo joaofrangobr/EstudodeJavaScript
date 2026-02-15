93fd6276c5d3b48a998697501067b4e37345fe28
joaofrangobr <joaorobertozucon85@gmail.com>
2026-02-15 10:40:54 -0300

    Add simple login.js with hardcoded creds
    Add login.js containing a basic username/password check using hardcoded values. The file defines login="joao1" and password="123456" but the check compares login to "joao", so the condition will fail. Storing plaintext credentials in source is insecure; use environment variables or a proper authentication mechanism instead.

---
+++ login.js
@@ -0,0 +1,8 @@
+const login = "joao"; // O código acima define uma variável chamada "login" e atribui a ela o valor "joao1". Essa variável pode ser usada posteriormente para verificar se o login do usuário é correto ou não.
+const password = "123456";
+
+if (login === "joao" && password === "123456") {
+    console.log("Usuário conectado")
+} else {
+    console.log("login ou senha incorretos")
+} // O código acima verifica se o valor da variável "login" é igual a "joao" e se o valor da variável "password" é igual a "123456". Se ambas as condições forem verdadeiras, ele imprime "Usuário conectado". Caso contrário, ele imprime "login ou senha incorretos".
